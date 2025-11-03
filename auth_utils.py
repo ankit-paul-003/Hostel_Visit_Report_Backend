882fafb4b6a92ecb8753ef21a3e33d487a27f044
paul-ankit <apaul6610@gmail.com>
2025-11-03 19:44:56 +0530

    Cleanup: Resolve merge conflicts in auth_utils.py

--- auth_utils.py
+++ auth_utils.py
@@ -1,20 +1,12 @@
-<<<<<<< HEAD
-=======
 from dotenv import load_dotenv
 import os
 load_dotenv()
 
->>>>>>> 4efd6c871f2d2a90e6126f0e1cf9fc57364d4534
 import datetime
-
 import jwt
 
 # Use an env var in production
-<<<<<<< HEAD
-SECRET_KEY = "your_secret_key_here_change_in_prod"
-=======
 SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
->>>>>>> 4efd6c871f2d2a90e6126f0e1cf9fc57364d4534
 
 def generate_token(user_type, username, hours_valid=3):
     payload = {
